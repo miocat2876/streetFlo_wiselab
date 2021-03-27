0d30e2276bbc6b3779d841b391336cd90abcfbc0
miocat <kkm1640@naver.com>
2021-03-27 11:46:27 +0900

    cors 테스트

--- src/main/java/com/streetflo/miocat/config/CorsConfig.java
+++ src/main/java/com/streetflo/miocat/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대해서
-                .allowedOrigins("http://localhost:63342"); //허용할 오리진들
+                .allowedOrigins("*"); //허용할 오리진들
     }
 }
 
