63075568c3a041b9b792625f68655872c27656a1
miocat <kkm1640@naver.com>
2021-03-27 11:40:18 +0900

    cors 테스트

--- src/main/java/com/streetflo/miocat/controller/TestController.java
+++ src/main/java/com/streetflo/miocat/controller/TestController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,6 +19,7 @@ public class TestController {
     @Autowired
     private TestService test;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/test")
     @ResponseBody
     public String test(){
