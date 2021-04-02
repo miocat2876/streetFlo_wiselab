d7fad696ac0258149fa444f2fb8309d60a3d6295
miocat <kkm1640@naver.com>
2021-04-02 23:55:58 +0900

    타일즈1

--- src/main/java/com/streetflo/miocat/controller/TestController.java
+++ src/main/java/com/streetflo/miocat/controller/TestController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -23,6 +24,18 @@ public class TestController {
 
         return "index.tile";
         
+    }
+
+    @GetMapping("/test2")
+    @ResponseBody
+    public String test2(){
+
+        System.out.println("테스트화면");
+
+        logger.info("로그나옵니까?");
+
+        return "index.tile";
+
     }
 
 
