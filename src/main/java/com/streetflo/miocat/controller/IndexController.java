37554fd210792c03f9034b9174bd8f0950650617
jean suh <jeansuh97@gmail.com>
2021-04-04 02:09:14 +0900

    2021.04.04 - mustache 오류 해결 / 컨트롤러 매핑 해결

--- src/main/java/com/streetflo/miocat/controller/IndexController.java
+++ src/main/java/com/streetflo/miocat/controller/IndexController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 // 페이지에 관련된 컨트롤러는 모두 여기에서 사용
