2ceefc6d73bd583cb6f8ae788b1ca7a28bcf5f62
miocat <kkm1640@naver.com>
2021-04-11 12:10:32 +0900

    ajax 테스트(시큐리티)

--- src/main/java/com/streetflo/miocat/controller/TestController.java
+++ src/main/java/com/streetflo/miocat/controller/TestController.java
@@ -47,22 +47,11 @@ public class TestController {
 
         System.out.println("테스트화면2");
 
-        return "{" +
-                "    date : 2021/02," +
-                "schedule : {24: [{startTime : 12:00," +
-                "                    endTime : 14:00," +
-                "                      genre : 브레이크댄스 기초," +
-                "                      level : Beginer," +
-                "                  subscribe : n}," +
-                "                 {startTime : 16:00," +
-                "                    endTime : 18:00," +
-                "                      genre : 브레이크댄스 고급," +
-                "                      level : Master," +
-                "                  subscribe : n}" +
-                "                ]" +
-                "            }" +
-                "             " +
-                "}";
+        String data = "{'date' : '2021/02','schedule' : {'24': [{'startTime' : '12:00','endTime' : '14:00','genre' : '브레이크댄스 기초','level' : 'Beginer','subscribe' : 'n'},{'startTime' : '16:00','endTime' : '18:00','genre' : '브레이크댄스 고급','level' : 'Master','subscribe' : 'n'}]}}";
+
+        return data;
+
+
 
     }
 
