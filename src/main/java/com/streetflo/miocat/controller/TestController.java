19b99d2405bbe094cc177fad6f30fe23371fcdf5
miocat <kkm1640@naver.com>
2021-04-11 10:55:16 +0900

    ajax 테스트(시큐리티)

--- src/main/java/com/streetflo/miocat/controller/TestController.java
+++ src/main/java/com/streetflo/miocat/controller/TestController.java
@@ -66,5 +66,45 @@ public class TestController {
 
     }
 
+    @GetMapping("/test6")
+    @ResponseBody
+    public String test6(){
+
+        System.out.println("테스트화면2");
+
+        return "[{noticeGenre : 테스트 공지사항1," +
+                "            date : 2021/04/11," +
+                "        content : '테스트1'" +
+                "}," +
+                "{noticeGenre : 테스트 공지사항2," +
+                "            date : 2021/04/11," +
+                "        content : '테스트2'" +
+                "}]";
+
+    }
+
+    @GetMapping("/test7")
+    @ResponseBody
+    public String test7(){
+
+        System.out.println("테스트화면2");
+
+        return "  [{name : 김하연," +
+                "    genre : 크럼핑," +
+                "     level : Master," +
+                "content1 : 유익한 수업, 재밌는 커리큘럼," +
+                "content2 : 다양한 강사 선생님," +
+                "content3 : 좋은 시설}," +
+                "  {name : 김경민," +
+                "    genre : 미어캣," +
+                "     level : Master," +
+                "content1 : 테스트," +
+                "content2 : ''," +
+                "content3 : ''}" +
+                "" +
+                "]";
+
+    }
+
 
 }
