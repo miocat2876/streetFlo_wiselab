2ff19d7dd4088c390246f44892f39487f8ea882a
miocat <kkm1640@naver.com>
2021-08-31 23:04:23 +0900

    prettier 셋팅

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,24 +1,29 @@
-
 module.exports = {
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   extends: [
     // https://eslint.vuejs.org/rules/#priority-b-strongly-recommended-improving-readability-for-vue-js-3-x
-    'plugin:vue/strongly-recommended'
+    'plugin:vue/strongly-recommended',
   ],
   rules: {
-    // https://eslint.vuejs.org/rules/html-closing-bracket-newline.html
-    'vue/html-closing-bracket-newline': ['error', {
-      multiline: 'never'
-    }],
+    //https://eslint.vuejs.org/rules/html-closing-bracket-newline.html
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        multiline: 'never',
+      },
+    ],
     // https://eslint.vuejs.org/rules/html-self-closing.html
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'always', // E.g, <br />
-        normal: 'never', // E.g, <div></div>
-        component: 'never' // E.g, <my-comp></my-comp>
-      }
-    }]
-  }
-}
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always', // E.g, <br />
+          normal: 'never', // E.g, <div></div>
+          component: 'never', // E.g, <my-comp></my-comp>
+        },
+      },
+    ],
+  },
+};
--- front/src/views/Calender.vue
+++ front/src/views/Calender.vue
@@ -4,22 +4,9 @@
       <div class="left">
         <div class="top">
           <h1>Calendar</h1>
-          <h3>
-            <a class="mon_before" @click="calenderPrev"></a
-            >{{ computedMonthNames }}, {{ year
-            }}<a class="mon_after" @click="calenderNext"></a>
-          </h3>
-          <a
-            class="add_bt"
-            v-if="state == 'mypage'"
-            v-bind:class="[addBt ? 'on' : '']"
-            @click="addBt = !addBt"
-            >Add Class</a
-          >
-          <ul
-            class="add_class"
-            v-bind:style="[addBt ? 'display : block' : 'display : none']"
-          >
+          <h3><a class="mon_before" @click="calenderPrev"></a>{{ computedMonthNames }}, {{ year }}<a class="mon_after" @click="calenderNext"></a></h3>
+          <a class="add_bt" v-if="state === 'mypage'" :class="[addBt ? 'on' : '']" @click="addBt = !addBt">Add Class</a>
+          <ul class="add_class" :style="[addBt ? 'display : block' : 'display : none']">
             <li class="date">
               <div class="title_box">
                 <h4>Date:</h4>
@@ -37,66 +24,31 @@
                   <label @click="computedEverydayToggle()">매일</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="mon"
-                    value="0"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="mon" value="0" v-model="checkedDays" />
                   <label for="mon">일</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="tue"
-                    value="1"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="tue" value="1" v-model="checkedDays" />
                   <label for="tue">월</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="wed"
-                    value="2"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="wed" value="2" v-model="checkedDays" />
                   <label for="wed">화</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="thu"
-                    value="3"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="thu" value="3" v-model="checkedDays" />
                   <label for="thu">수</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="fri"
-                    value="4"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="fri" value="4" v-model="checkedDays" />
                   <label for="fri">목</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="sat"
-                    value="5"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="sat" value="5" v-model="checkedDays" />
                   <label for="sat">금</label>
                 </li>
                 <li>
-                  <input
-                    type="checkbox"
-                    id="sun"
-                    value="6"
-                    v-model="checkedDays"
-                  />
+                  <input type="checkbox" id="sun" value="6" v-model="checkedDays" />
                   <label for="sun">토</label>
                 </li>
               </ul>
@@ -172,21 +124,11 @@
           </ul>
         </div>
       </div>
-      <div
-        class="right"
-        v-bind:style="[state != 'mypage' ? 'min-height: auto' : '']"
-      >
+      <div class="right" :style="[state !== 'mypage' ? 'min-height: auto' : '']">
         <table>
           <tr v-for="n in Math.ceil(computedMakeCaledar.length / 7)" :key="n">
-            <td
-              v-bind:class="[item.schedule ? 'able' : '']"
-              v-for="item in computedMakeCaledar.slice((n - 1) * 7, 7 * n)"
-              :key="item.date"
-            >
-              <p
-                v-bind:class="[item.schedule ? 'calendar_tooltip' : '']"
-                v-bind:style="[state != 'mypage' ? 'padding: 4px' : '']"
-              >
+            <td :class="[item.schedule ? 'able' : '']" v-for="item in computedMakeCaledar.slice((n - 1) * 7, 7 * n)" :key="item.date">
+              <p :class="[item.schedule ? 'calendar_tooltip' : '']" :style="[state !== 'mypage' ? 'padding: 4px' : '']">
                 {{ item.day }}
               </p>
               <div id="calendar" v-if="item.schedule">
@@ -197,24 +139,19 @@
                   <ul>
                     <li v-for="schedule in item.schedule" :key="schedule.key">
                       <h3>
-                        {{ schedule.startTime }}-{{ schedule.endTime }} 상태 :
+                        {{ schedule.startTime }}-{{ schedule.endTime }}
+                        상태 :
                         {{ schedule.type }}
                       </h3>
                       <p>[{{ schedule.level }}]</p>
                       <span>
-                        <span v-if="state != 'mypage'">
-                          Class Level: {{ schedule.level }}</span
+                        <span v-if="state !== 'mypage'">
+                          Class Level:
+                          {{ schedule.level }}</span
                         >
-                        <span v-if="state == 'mypage'">{{
-                          schedule.content
-                        }}</span>
+                        <span v-if="state === 'mypage'">{{ schedule.content }}</span>
                       </span>
-                      <a
-                        v-if="state != 'mypage'"
-                        @click="scheduleSubscribeAdd(schedule)"
-                      >
-                        임시 상세등록
-                      </a>
+                      <a v-if="state !== 'mypage'" @click="scheduleSubscribeAdd(schedule)"> 임시 상세등록 </a>
                     </li>
                   </ul>
                 </div>
@@ -228,172 +165,159 @@
 </template>
 
 <script>
-import { scheduleFind, scheduleAdd, scheduleSubscribeAdd } from "../api";
+  import {scheduleFind, scheduleAdd, scheduleSubscribeAdd} from '../api';
 
-export default {
-  props: {
-    propsState: String,
-  },
-  async created() {
-    let agrument = {
-      year: this.year,
-      month: this.month + 1,
-      memberSeq: this.id,
-      date: `${this.year}-${this.month}-${this.day}`,
-    };
-    this.state = "mypage";
-    try {
-      console.log(agrument);
-      const { data: result } = await scheduleFind(agrument);
-      console.log("====>", result);
-      this.data = result;
-    } catch (e) {
-      console.log(e);
-    }
-  },
-  data: function () {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-
-    return {
-      id: 4,
-      day: 1,
-      year: year,
-      month: month + 1,
-      data: [],
-      addBt: false,
-      state: this.propsState,
-      startDay: "",
-      endDay: "",
-      checkedDays: [],
-      everydayToggle: false,
-      startTime: "",
-      endTime: "",
-      location: "",
-      content: "",
-      level: "",
-    };
-  },
-  components: {},
-  computed: {
-    computedMakeCaledar() {
-      return this.data;
+  export default {
+    props: {
+      propsState: String,
     },
-    computedMonthNames() {
-      const monthNames = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-      return monthNames[this.month - 1];
-    },
-  },
-
-  methods: {
-    goBack(a) {
-      console.log(a);
-    },
-    async calenderPrev() {
-      if (this.month == 1) {
-        this.year -= 1;
-        this.month = 12;
-      } else {
-        this.month -= 1;
-      }
-
+    async created() {
       let agrument = {
         year: this.year,
         month: this.month + 1,
         memberSeq: this.id,
         date: `${this.year}-${this.month}-${this.day}`,
       };
-
-      const { data: result } = await scheduleFind(agrument);
-      console.log(result);
-      this.data = result;
-    },
-    async calenderNext() {
-      if (this.month == 12) {
-        this.year += 1;
-        this.month = 1;
-      } else {
-        this.month += 1;
-      }
-
-      let agrument = {
-        year: this.year,
-        month: this.month + 1,
-        memberSeq: this.id,
-        date: `${this.year}-${this.month}-${this.day}`,
-      };
-
-      console.log(agrument);
-      const { data: result } = await scheduleFind(agrument);
-      console.log(result);
-      this.data = result;
-    },
-    async scheduleAdd() {
-      let agrument = {
-        memberSeq: this.id,
-        content: this.content,
-        schedule: this.schedule,
-        distinction: this.distinction,
-        startTime: this.startTime,
-        endTime: this.endTime,
-        startDay: this.startDay,
-        endDay: this.endDay,
-        genre: this.genre,
-        level: this.level,
-        week: this.checkedDays,
-      };
-
-      console.log(agrument);
-
-      const { data: addResult } = await scheduleAdd(agrument);
-
-      console.log(addResult);
-
-      let agrument2 = {
-        year: this.year,
-        month: this.month + 1,
-        memberSeq: this.id,
-        date: `${this.year}-${this.month}-${this.day}`,
-      };
-      const { data: result } = await scheduleFind(agrument2);
-      console.log(result);
-      this.data = result;
-    },
-    computedEverydayToggle() {
-      this.everydayToggle = !this.everydayToggle;
-
-      if (this.everydayToggle) {
-        this.checkedDays = [0, 1, 2, 3, 4, 5, 6];
-      } else {
-        this.checkedDays = [];
+      this.state = 'mypage';
+      try {
+        console.log(agrument);
+        const {data: result} = await scheduleFind(agrument);
+        console.log('====>', result);
+        this.data = result;
+      } catch (e) {
+        console.log(e);
       }
     },
-    async scheduleSubscribeAdd(e) {
-      let agrument = {
-        memberSeq: this.id,
-        scheduleSeq: e.seq,
-        subscribeDt: e.date,
-      };
+    data: function () {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth();
 
-      const { data: result } = await scheduleSubscribeAdd(agrument);
-      console.log(result);
+      return {
+        id: 4,
+        day: 1,
+        year: year,
+        month: month + 1,
+        data: [],
+        addBt: false,
+        state: this.propsState,
+        startDay: '',
+        endDay: '',
+        checkedDays: [],
+        everydayToggle: false,
+        startTime: '',
+        endTime: '',
+        location: '',
+        content: '',
+        level: '',
+      };
     },
-  },
-};
+    components: {},
+    computed: {
+      computedMakeCaledar() {
+        return this.data;
+      },
+      computedMonthNames() {
+        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return monthNames[this.month - 1];
+      },
+    },
+
+    methods: {
+      goBack(a) {
+        console.log(a);
+      },
+      async calenderPrev() {
+        if (this.month === 1) {
+          this.year -= 1;
+          this.month = 12;
+        } else {
+          this.month -= 1;
+        }
+
+        let agrument = {
+          year: this.year,
+          month: this.month + 1,
+          memberSeq: this.id,
+          date: `${this.year}-${this.month}-${this.day}`,
+        };
+
+        const {data: result} = await scheduleFind(agrument);
+        console.log(result);
+        this.data = result;
+      },
+      async calenderNext() {
+        if (this.month === 12) {
+          this.year += 1;
+          this.month = 1;
+        } else {
+          this.month += 1;
+        }
+
+        let agrument = {
+          year: this.year,
+          month: this.month + 1,
+          memberSeq: this.id,
+          date: `${this.year}-${this.month}-${this.day}`,
+        };
+
+        console.log(agrument);
+        const {data: result} = await scheduleFind(agrument);
+        console.log(result);
+        this.data = result;
+      },
+      async scheduleAdd() {
+        let agrument = {
+          memberSeq: this.id,
+          content: this.content,
+          schedule: this.schedule,
+          distinction: this.distinction,
+          startTime: this.startTime,
+          endTime: this.endTime,
+          startDay: this.startDay,
+          endDay: this.endDay,
+          genre: this.genre,
+          level: this.level,
+          week: this.checkedDays,
+        };
+
+        console.log(agrument);
+
+        const {data: addResult} = await scheduleAdd(agrument);
+
+        console.log(addResult);
+
+        let agrument2 = {
+          year: this.year,
+          month: this.month + 1,
+          memberSeq: this.id,
+          date: `${this.year}-${this.month}-${this.day}`,
+        };
+        const {data: result} = await scheduleFind(agrument2);
+        console.log(result);
+        this.data = result;
+      },
+      computedEverydayToggle() {
+        this.everydayToggle = !this.everydayToggle;
+
+        if (this.everydayToggle) {
+          this.checkedDays = [0, 1, 2, 3, 4, 5, 6];
+        } else {
+          this.checkedDays = [];
+        }
+      },
+      async scheduleSubscribeAdd(e) {
+        let agrument = {
+          memberSeq: this.id,
+          scheduleSeq: e.seq,
+          subscribeDt: e.date,
+        };
+
+        const {data: result} = await scheduleSubscribeAdd(agrument);
+        console.log(result);
+      },
+    },
+  };
 </script>
 
 <style scoped lang="scss"></style>
