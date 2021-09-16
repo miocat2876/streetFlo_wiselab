79fe5cc1d6e3c9a9547e43e13e77547254776af9
hjlee <rshjlee@gmail.com>
2021-09-16 20:49:58 +0900

    111

---
+++ src/main/java/com/streetflo/miocat/service/rest/ProfileService.java
@@ -0,0 +1,18 @@
+package com.streetflo.miocat.service.rest;
+
+import com.streetflo.miocat.dto.TestDto;
+import com.streetflo.miocat.dto.rest.CalenderDto;
+import com.streetflo.miocat.dto.table.ProfileDto;
+import com.streetflo.miocat.dto.table.ScheduleSubscribeDto;
+
+import java.util.List;
+
+public interface CalenderService {
+
+    
+
+    int scheduleAdd(ProfileDto dto);
+
+    
+
+}
