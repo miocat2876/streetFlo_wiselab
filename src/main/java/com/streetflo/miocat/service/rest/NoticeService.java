6e8e36b2715e606821e4d44a9ab71ba72f476f97
jeansuh42 <jeansuh97@gmail.com>
2021-09-22 22:29:50 +0900

    Merge Main

--- src/main/java/com/streetflo/miocat/service/rest/NoticeService.java
+++ src/main/java/com/streetflo/miocat/service/rest/NoticeService.java
@@ -1,13 +1,7 @@
-package com.streetflo.miocat.service.rest;
-
 import com.streetflo.miocat.dto.TestDto;
-
-import java.util.List;
 
 public interface NoticeService {
 
-    List<NoticeDto> noticeFind(NoticeDto noticeDto);
-
-    int NoticeAdd(NoticeDto dto);
+    TestDto test();
 
 }
--- src/main/java/com/streetflo/miocat/service/rest/impl/NoticeServiceImpl.java
+++ src/main/java/com/streetflo/miocat/service/rest/impl/NoticeServiceImpl.java
@@ -1,33 +1,19 @@
 package com.streetflo.miocat.service.rest.impl;
 
+import com.streetflo.miocat.dto.TestDto;
+import com.streetflo.miocat.dao.TestMapper;
+import com.streetflo.miocat.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service("NoticeService")
-public class NoticeServiceImpl implements NoticeService {
+@Service
+public class NoticeServiceImpl implements TestService {
 
     @Autowired
-    private NoticeMapper NoticeMapper;
+    private TestMapper testMapper;
 
     @Override
-    public List<NoticeDto> NoticeList(NoticeDto NoticeDto) {
-        List<NoticeDto> NoticeList = NoticeMapper.NoticeFind(NoticeDto);
-        return NoticeList;
+    public TestDto test() {
+
+        return testMapper.test2();
     }
-
-    @Override
-    public int NoticeAdd(NoticeDto dto) {
-        int result = 0;
-        if(NoticeMapper.NoticeAdd(dto) != 0){
-            result = NoticeMapper.NoticeAdd(dto);
-        }
-        return result;
-    }
-
-
-
 }
