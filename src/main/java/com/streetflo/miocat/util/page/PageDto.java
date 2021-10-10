8b7d9b2fd7e0eb938d84b917d87073b7997f7fae
miocat <kkm1640@naver.com>
2021-10-10 19:47:31 +0900

    페이징 구현체 -> 디폴트 인터페이스 수정

--- src/main/java/com/streetflo/miocat/util/page/PageDto.java
+++ src/main/java/com/streetflo/miocat/util/page/PageDto.java
@@ -2,32 +2,23 @@ package com.streetflo.miocat.util.page;
 
 public interface PageDto {
 
+	int defaultViewData = 20;
 	public abstract int getParamCurrentPage();
 	public abstract int getParamViewData();
 	public abstract String getParamSearchCondition();
 	public abstract String getParamSearchValue();
 
-
-//			this.currentPage = bulider.currentPage == 0 ? 1 : bulider.currentPage;
-//		this.viewData = bulider.viewData == 0 ? defaultViewData : bulider.viewData;
-//		this.searchCondition = bulider.searchCondition == null ? "" : bulider.searchCondition;
-//		this.searchValue = bulider.searchValue == null ? "" : bulider.searchValue;
-
 	default public int getStartData() {
-		int defaultViewData = 20;
 		int currentPage = getParamCurrentPage() == 0 ? 1 : getParamCurrentPage();
 		int viewData = getParamViewData() == 0 ? defaultViewData : getParamViewData();
 		return (currentPage - 1) * viewData + 1;
 	}
 	default public int getEndData() {
-		int defaultViewData = 20;
 		int currentPage = getParamCurrentPage() == 0 ? 1 : getParamCurrentPage();
 		int viewData = getParamViewData() == 0 ? defaultViewData : getParamViewData();
-
 		return currentPage * viewData;
 	}
 	default public int getViewData() {
-		int defaultViewData = 20;
 		return getParamViewData() == 0 ? defaultViewData : getParamViewData();
 	}
 	default public String getSearchCondition() {
